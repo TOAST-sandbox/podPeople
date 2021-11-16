a92ac761d9c9f2dd3d097c374009f4b45214b0ac
Lee Katz - Aspen <gzu2@cdc.gov>
2021-11-16 15:59:36 -0500

    misc

--- src/ReadCleanBot.general.sh
+++ src/ReadCleanBot.general.sh
@@ -10,7 +10,9 @@ export PATH=$ROOT/../sra-human-scrubber-1.1.2021-05-05/bin:$PATH
 export PATH=$ROOT/../sra-human-scrubber-1.1.2021-05-05/scripts:$PATH
 
 function check {
+  # We'll do error handling outselves in this function
   set +e
+
   echo "Checking dependencies"
   echo "PATH: $PATH"
   for exe in bedtools bbsplitpairs.sh bowtie2 gcc samtools fasten_shuffle replaceReadsWithReference.pl scrub.sh; do
@@ -46,13 +48,15 @@ if [[ "$ARGC" -lt 3 || "$1" == "--help" || "$1" == "-help" || "$1" == "-h" ]] ; 
    
    ===========
    
-   Usage: ./ReadCleanBot.general <in-dir> <out-dir> <path-to-ref>
+   Usage: ./ReadCleanBot.general [--check] <in-dir> <out-dir> <path-to-ref>
 
    in-dir  Path to the directory containing the raw reads pairs in fastq format with filenames like 'name_S1_L001_R[12]_001.fastq'.
    
    out-dir  Path to the new output directory.
        
    path-to-ref  Path to the human reference genome assembly fasta (must be indexed for bowtie2)
+
+   --check  Check dependencies before running the algorithm. Exit code is 0 for no error; 1 for missing dependency.
    
    ===========
    
@@ -97,7 +101,6 @@ function anonymize {
 # step-2: Human read scrubber and repair broken pairs
 
   scrb=$outdir/03.fastq-scrubbed/$(basename $intlv .interleaved.fastq).scrubbed.fastq;
-  echo "scrubbed    $scrb"
   tmp=${scrb/.scrubbed.fastq/.tmp.fastq}
   single=${scrb/.scrubbed.fastq/.single.fastq}
   rtmp=${scrb/.scrubbed.fastq/.rtmp.fastq}
@@ -109,7 +112,6 @@ function anonymize {
     cat $intlv.clean | sed 's/\s/\//g' | sed 's/\:N\:0.*//g' > $tmp;
     cat $intlv.removed_spots | sed 's/\s/\//g' | sed 's/\:N\:0.*//g' > $rtmp;
 
-    set -x
     bbsplitpairs.sh in=$tmp out=$scrb outs=$single fint
     bbsplitpairs.sh in=$rtmp out=$rmvd outs=$rsingle fint
 
@@ -132,7 +134,7 @@ function anonymize {
 
 # step-4: Replace patient sequence with corresponding T2T reference sequence
 
-  rfq=$outdir/05.fastq-replaceref/$(basename $bm bam).fastq;
+  rfq=$outdir/05.fastq-replaceref/$(basename $bm .bam).fastq;
 
   if [ ! -f $rfq ];then
     replaceReadsWithReference.pl \
@@ -146,28 +148,30 @@ function anonymize {
 
 # step-5: Combine, shuffle, separate into R1 and R2 for SRA submission
 
-  f1=$(basename $r1);
-  f2=$(basename $r2);
+  f1=$outdir/06.fastq-forSRA/$(basename $r1);
+  f2=$outdir/06.fastq-forSRA/$(basename $r2);
 
-  if [ ! -f $outdir/06.fastq-forSRA/$f1 ];then
-    cat $outdir/03.fastq-scrubbed/$scrb $outdir/05.fastq-replaceref/$rfq | sed 's/\sreplaced.*//g' | \
+  if [ ! -f $f1 ];then
+    cat $scrb $rfq | sed 's/\sreplaced.*//g' | \
     fasten_randomize -n "${NSLOTS}" -p | \
     fasten_shuffle -n "${NSLOTS}" -d \
-    -1 $outdir/06.fastq-forSRA/$f1 -2 $outdir/06.fastq-forSRA/$f2;
+    -1 $f1.tmp -2 $f2.tmp;
+
+    mv $f1.tmp $f1
+    mv $f2.tmp $f2
   fi
   echo -e "\tReads prepared for SRA....[x]";
 
-
 # step-6: Summarize some count metrics
 
-  sum=$(basename $intlv .fastq).stats.tsv;
+  sum=$outdir/07.summary/$(basename $intlv .fastq).stats.tsv;
 
-  RAW=$(wc -l $outdir/02.fastq-interleaved/$intlv | awk '{print $1/8}');
-  SCRpass=$(wc -l $outdir/03.fastq-scrubbed/$scrb | awk '{print $1/8}');
-  T2Treplaced=$(wc -l $outdir/05.fastq-replaceref/$rfq | awk '{print $1/8}');
-  TOTsra=$(wc -l $outdir/06.fastq-forSRA/$f1 | awk '{print $1/4}');
+  RAW=$(wc -l $intlv | awk '{print $1/8}');
+  SCRpass=$(wc -l $scrb | awk '{print $1/8}');
+  T2Treplaced=$(wc -l $rfq | awk '{print $1/8}');
+  TOTsra=$(wc -l $f1 | awk '{print $1/4}');
   FHS=$(echo "scale=4; $T2Treplaced/$TOTsra" | bc | awk '{printf "%.4f", $0}' );
-  SCRfail=$(wc -l $outdir/03.fastq-scrubbed/$rmvd | awk '{print $1/8}');
+  SCRfail=$(wc -l $rmvd | awk '{print $1/8}');
 
   if [ "$RAW" -gt "$SCRpass" ];then
           T2F=$(echo "scale=4; $T2Treplaced/$SCRfail" | bc | awk '{printf "%.4f", $0}' );
@@ -175,8 +179,8 @@ function anonymize {
           T2F=0;
   fi
 
-  echo -e "ReadID\tRaw_pairs\tScrub_pass\tScrub_remove\tReplaced_pairs\tReplaced_frac\tTotal_pairs_final\tHuman_frac_final" > ./07.summary/$sum;
-  echo -e "$readid\t$RAW\t$SCRpass\t$SCRfail\t$T2Treplaced\t$T2F\t$TOTsra\t$FHS" >> ./07.summary/$sum;
+  echo -e "ReadID\tRaw_pairs\tScrub_pass\tScrub_remove\tReplaced_pairs\tReplaced_frac\tTotal_pairs_final\tHuman_frac_final" > $sum;
+  echo -e "$readid\t$RAW\t$SCRpass\t$SCRfail\t$T2Treplaced\t$T2F\t$TOTsra\t$FHS" >> $sum;
 
 
 echo -e "\tPipeline finished!\n";
