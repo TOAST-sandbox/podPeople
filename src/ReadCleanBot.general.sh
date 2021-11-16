22778ae889d13f0894e540f185bc4fe71cf37f48
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2021-11-16 15:32:25 -0500

    converted tabs to 2 spaces

--- src/ReadCleanBot.general.sh
+++ src/ReadCleanBot.general.sh
@@ -48,11 +48,11 @@ if [[ "$ARGC" -lt 3 || "$1" == "--help" || "$1" == "-help" || "$1" == "-h" ]] ; 
    
    Usage: ./ReadCleanBot.general <in-dir> <out-dir> <path-to-ref>
 
-   in-dir	Path to the directory containing the raw reads pairs in fastq format with filenames like 'name_S1_L001_R[12]_001.fastq'.
+   in-dir  Path to the directory containing the raw reads pairs in fastq format with filenames like 'name_S1_L001_R[12]_001.fastq'.
    
-   out-dir	Path to the new output directory.
-   		
-   path-to-ref	Path to the human reference genome assembly fasta (must be indexed for bowtie2)
+   out-dir  Path to the new output directory.
+       
+   path-to-ref  Path to the human reference genome assembly fasta (must be indexed for bowtie2)
    
    ===========
    
@@ -62,41 +62,41 @@ fi ;
 
 ### make output directories if needed
 if [[ ! -d "$2" ]]; then
-	mkdir $2;
-	mkdir $2/02.fastq-interleaved
-	mkdir $2/03.fastq-scrubbed
-	mkdir $2/04.bam
-	mkdir $2/05.fastq-replaceref
-	mkdir $2/06.fastq-forSRA
-	mkdir $2/07.summary
+  mkdir $2;
+  mkdir $2/02.fastq-interleaved
+  mkdir $2/03.fastq-scrubbed
+  mkdir $2/04.bam
+  mkdir $2/05.fastq-replaceref
+  mkdir $2/06.fastq-forSRA
+  mkdir $2/07.summary
 fi
 
 #cd "${o}"
 
 ### wrap pipelin into a function 
 function anonymize {
-	r1=$1;
+  r1=$1;
   outdir=$2
-	ref=$3;		
-	readid=$(basename $r1 | sed -e 's/_L[0-9]*.*//g' -e 's/_1.f.*//');
+  ref=$3;    
+  readid=$(basename $r1 | sed -e 's/_L[0-9]*.*//g' -e 's/_1.f.*//');
 
 # step-1: Interleave R1 and R2
 
-	r2=$(echo $r1 | sed -e 's/_R1_/_R2_/g' -e 's/_1.f/_2.f/');
+  r2=$(echo $r1 | sed -e 's/_R1_/_R2_/g' -e 's/_1.f/_2.f/');
   intlv="$outdir/02.fastq-interleaved/$readid.interleaved.fastq"
-	#intlv=$(basename $r1 | sed 's/R1_001/interleaved/g');
+  #intlv=$(basename $r1 | sed 's/R1_001/interleaved/g');
 
   echo "R1          $r1"
   echo "R2          $r2"
   echo "interleaved $intlv"
 
-	if [ ! -f $intlv ];then
-		zcat $r1 $r2 | fasten_shuffle > $intlv;
-	fi
+  if [ ! -f $intlv ];then
+    zcat $r1 $r2 | fasten_shuffle > $intlv;
+  fi
 
 # step-2: Human read scrubber and repair broken pairs
 
-	scrb=$outdir/03.fastq-scrubbed/$(basename $intlv .interleaved.fastq).scrubbed.fastq;
+  scrb=$outdir/03.fastq-scrubbed/$(basename $intlv .interleaved.fastq).scrubbed.fastq;
   echo "scrubbed    $scrb"
   tmp=${scrb/.scrubbed.fastq/.tmp.fastq}
   single=${scrb/.scrubbed.fastq/.single.fastq}
@@ -104,72 +104,72 @@ function anonymize {
     rsingle=${scrb/.scrubbed.fastq/.rsingle.fastq}
     rmvd=${scrb/.scrubbed.fastq/.removed.fastq}
 
-	if [ ! -f $scrb ];then
-		scrub.sh -r $intlv;
+  if [ ! -f $scrb ];then
+    scrub.sh -r $intlv;
     cat $intlv.clean | sed 's/\s/\//g' | sed 's/\:N\:0.*//g' > $tmp;
     cat $intlv.removed_spots | sed 's/\s/\//g' | sed 's/\:N\:0.*//g' > $rtmp;
 
     set -x
-		bbsplitpairs.sh in=$tmp out=$scrb outs=$single fint
+    bbsplitpairs.sh in=$tmp out=$scrb outs=$single fint
         bbsplitpairs.sh in=$rtmp out=$rmvd outs=$rsingle fint
 
-	fi
-	echo -e "\tScrubbed....[x]";
+  fi
+  echo -e "\tScrubbed....[x]";
 
 # step-3: Bowtie2 map to human reference
 
-	bm=$outdir/04.bam/$(basename $intlv .fastq).t2t.bam;
+  bm=$outdir/04.bam/$(basename $intlv .fastq).t2t.bam;
 
-	if [ ! -f $bm ];then
-		bowtie2 \
-		-X 1000 \
-		-p "${NSLOTS}" -x $ref \
+  if [ ! -f $bm ];then
+    bowtie2 \
+    -X 1000 \
+    -p "${NSLOTS}" -x $ref \
                 --interleaved $rmvd | samtools view -b -F 12 > $bm.tmp;
 
     mv $bm.tmp $bm
-	fi
-	echo -e "\tMapped to reference....[x]";
+  fi
+  echo -e "\tMapped to reference....[x]";
 
 # step-4: Replace patient sequence with corresponding T2T reference sequence
 
-	rfq=$outdir/05.fastq-replaceref/$(basename $bm bam).fastq;
+  rfq=$outdir/05.fastq-replaceref/$(basename $bm bam).fastq;
 
-	if [ ! -f $rfq ];then
-		replaceReadsWithReference.pl \
-		$ref $bm 1> $rfq.tmp 2> $rfq.stderr.log;
+  if [ ! -f $rfq ];then
+    replaceReadsWithReference.pl \
+    $ref $bm 1> $rfq.tmp 2> $rfq.stderr.log;
     mv $rfq.tmp $rfq
 
     echo TODO
     exit 43
-		
+    
     # Not sure what this step is, because $rfq.clean doesn't exist
-		#mv $outdir/05.fastq-replaceref/$rfq.clean $outdir/05.fastq-replaceref/$rfq.tmp
+    #mv $outdir/05.fastq-replaceref/$rfq.clean $outdir/05.fastq-replaceref/$rfq.tmp
 
     bbsplitpairs.sh in=$outdir/05.fastq-replaceref/$rfq.tmp out=$outdir/05.fastq-replaceref/$rfq.clean outs=$outdir/05.fastq-replaceref/$rfq.single fint
 
-	fi
-	echo -e "\tRead sequences replaced....[x]";
+  fi
+  echo -e "\tRead sequences replaced....[x]";
 
 
 # step-5: Combine, shuffle, separate into R1 and R2 for SRA submission
 
-	f1=$(basename $r1);
-	f2=$(basename $r2);
+  f1=$(basename $r1);
+  f2=$(basename $r2);
 
-	if [ ! -f $outdir/06.fastq-forSRA/$f1 ];then
-		cat $outdir/03.fastq-scrubbed/$scrb $outdir/05.fastq-replaceref/$rfq | sed 's/\sreplaced.*//g' | \
-		fasten_randomize -n "${NSLOTS}" -p | \
-		fasten_shuffle -n "${NSLOTS}" -d \
-		-1 $outdir/06.fastq-forSRA/$f1 -2 $outdir/06.fastq-forSRA/$f2;
-	fi
-	echo -e "\tReads prepared for SRA....[x]";
+  if [ ! -f $outdir/06.fastq-forSRA/$f1 ];then
+    cat $outdir/03.fastq-scrubbed/$scrb $outdir/05.fastq-replaceref/$rfq | sed 's/\sreplaced.*//g' | \
+    fasten_randomize -n "${NSLOTS}" -p | \
+    fasten_shuffle -n "${NSLOTS}" -d \
+    -1 $outdir/06.fastq-forSRA/$f1 -2 $outdir/06.fastq-forSRA/$f2;
+  fi
+  echo -e "\tReads prepared for SRA....[x]";
 
 
 # step-6: Summarize some count metrics
 
-	sum=$(basename $intlv .fastq).stats.tsv;
+  sum=$(basename $intlv .fastq).stats.tsv;
 
-	RAW=$(wc -l $outdir/02.fastq-interleaved/$intlv | awk '{print $1/8}');
+  RAW=$(wc -l $outdir/02.fastq-interleaved/$intlv | awk '{print $1/8}');
         SCRpass=$(wc -l $outdir/03.fastq-scrubbed/$scrb | awk '{print $1/8}');
         T2Treplaced=$(wc -l $outdir/05.fastq-replaceref/$rfq | awk '{print $1/8}');
         TOTsra=$(wc -l $outdir/06.fastq-forSRA/$f1 | awk '{print $1/4}');
@@ -199,7 +199,7 @@ echo -e "\nAnonymizing host reads...";
 #for i in $(find $1/*R1*fastq); do
 R1s=$(\ls -f1 $1/*_R1*.fastq.gz $1/*_1.fastq.gz || true)
 for i in $R1s; do
-	anonymize $i $2 $3;
+  anonymize $i $2 $3;
 
 done
 
